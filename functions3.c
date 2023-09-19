ce2ec119388e48c859f77aa8e439a8333550f0e0
Francis Macharia <francismureithim@gmail.com>
2023-09-19 22:52:29 +0300

    upper hex

--- functions3.c
+++ functions3.c
@@ -30,10 +30,41 @@ int pr_hex_lower(va_list args)
  *
  * Return: The number of characters printed
  */
-int pr_hex_upper(va_list args)
+int print_hex_upper(va_list args)
 {
-	va_arg(args, unsigned int);
-	return (0);
+	unsigned int num = va_arg(args, unsigned int);
+	int count = 0;
+	int remainder;
+	char hex_digit;
+	char hex_buffer[1024];
+	int i = 0;
+	char *reversed_hex = rev_st(hex_buffer);
+	
+	if (num == 0)
+	{
+		count += _putchar('0');
+		return count;
+	}
+	while (num > 0)
+	{
+		remainder = num % 16;
+		if (remainder < 10)
+		{
+			hex_digit = remainder + '0';
+		}
+		else
+		{
+			hex_digit = remainder - 10 + 'A';
+		}
+		hex_buffer[i++] = hex_digit;
+		num /= 16;
+	}
+	hex_buffer[i] = '\0';
+
+	count += puts(reversed_hex);
+	
+	free(reversed_hex);
+	return count;
 }
 
 /**
