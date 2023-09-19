f2fe823c40d04f35814eb9948f1c6bfed9b2c37c
Francis Macharia <francismureithim@gmail.com>
2023-09-19 17:34:14 +0300

    q3 trial

--- functions3.c
+++ functions3.c
@@ -35,3 +35,31 @@ int pr_hex_upper(va_list args)
 	va_arg(args, unsigned int);
 	return (0);
 }
+
+/**
+ * pr_unsigned - Handle %u specifier
+ * @args: The va_list of arguments
+ *
+ * Return
+ */
+
+int pr_unsigned(va_list args)
+{
+	unsigned int num = va_arg(args, unsigned int);
+	int length = 0;
+	unsigned int divisor = 1;
+	
+	while (num / divisor > 9)
+	{
+		divisor *= 10;
+	}
+	
+	while (divisor != 0)
+	{
+		length += _putchar('0' + num / divisor);
+		num %= divisor;
+		divisor /= 10;
+	}
+	
+	return length;
+}
